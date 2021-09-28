e893ac6a42a24ca9fd837a89ed07c1282148a74c
Alexander Palchikov <axelpal@gmail.com>
2021-09-28 23:40:24 +0500

    updated pre-push bash script

--- git-hooks/pre-push.sh
+++ git-hooks/pre-push.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 export TERM=xterm
-./vendor/bin/phpunit
+./artisan config:cache --env=testing
+./artisan test
 if [ $? -ne 0 ]; then
   echo "Fix your code before running tests and commit!"
   echo "Run following command to show in which files you've got problems:"
   echo "./artisan config:cache --env=testing && ./artisan test && ./artisan config:cache"
   exit 1;
 fi
+./artisan config:cache
 ./vendor/bin/phpstan analyze .
 if [ $? -ne 0 ]; then
   echo "Fix your code before running commit!"
